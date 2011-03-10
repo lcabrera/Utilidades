c76ef3d4e4810efea7f82b59842f601d0b38ca41
Luis Cabrera Sauco <icorrecam@gmail.com>
2011-03-10 21:10:31 +0000

    Subiendo cambios y pruebas: nada importante.

--- gestion de eventos/gestion_de_eventos.py
+++ gestion de eventos/gestion_de_eventos.py
@@ -49,7 +49,6 @@ __source__ = '$Source: $'
 
 # Variables:
 
-
 class Ejemplo:
 
     """
@@ -64,17 +63,21 @@ class Ejemplo:
         # importación de módulos
         import os
         import sys
+        import optparse
 
         # import logging
         import logging.config
 
-        # Variables:
+        # Declaramos algunas variables iniciales.
+        self.var_a = ''
+        self.var_b = []
+        self.nombre = ''
 
         # Parámetros a recibir:
         # Si no pasamos parametros de ningún tipo, la llamada al programa recibe
         # al menos el envio del nombre del fichero. Por eso hay que sumar uno a
         # esta función.
-        param = 0
+        param = 1
         param = param + 1
 
         if len(sys.argv) == int(param):
@@ -83,54 +86,100 @@ class Ejemplo:
         else:
             print 'Se han recibido %s argumentos.' % len(sys.argv)
             print 'Estos argumentos son: %s' % sys.argv
-            print '\nRedirigir a la función USAGE.\n'
-            sys.stderr.write('''\tArgumentos inválidos\n\n''')
+            sys.stderr.write('\t' + str(self.usage()) + '\n\n')
             sys.exit(1)
+
+        parser = optparse.OptionParser("usage: %prog [options] arg1 arg2")
+        parser.add_option(
+            "-H",
+            "--host",
+            dest="hostname",
+            default="127.0.0.1",
+            type="string",
+            help="specify hostname to run on")
+        parser.add_option(
+            "-p",
+            "--port",
+            dest="portnum",
+            default=80,
+            type="int",
+            help="port number to run on")
+
+        parser.add_option(
+            '--version',
+            dest="version",
+            default=1.0,
+            type="float",
+            )
+
+        (options, args) = parser.parse_args()
+        if len(args) != 2:
+            parser.error("incorrect number of arguments")
+
+        hostname = options.hostname
+        portnum = options.portnum
+
+        (options, remainder) = parser.parse_args()
+        print 'VERSION   :', options.version
+        print 'PUERTO    :', options.portnum
+        print 'HOST      :', options.hostname
+        print 'REMAINING :', remainder
 
         nombre_base = os.path.basename(os.path.splitext(sys.argv[0])[0])
         filename_log = nombre_base + '.log'
         filename_log_ini = nombre_base + '_logging.ini'
         log_nivel1 = 'nivel1'
 
-        logging.config.fileConfig(filename_log_ini)
-
-        # creamos un logger
-        logger = logging.getLogger(log_nivel1)
-
-        # Declaramos algunas variables iniciales.
-        self.var_a = ''
-        self.var_b = []
-        self.nombre = ''
-
         if os.path.exists(filename_log_ini) and os.path.isfile(filename_log_ini):
             logging.config.fileConfig(filename_log_ini)
-            logging.debug('Usaremos el fichero %s para el registro de eventos' % filename_log_ini)
+            # creamos un logger
+            self.log = logging.getLogger(log_nivel1)
+            self.log.info('Usaremos el fichero %s para el registro de eventos' % filename_log_ini)
         else:
-            logging.error('No ha sido posible encontrar el fichero de configuración del registro.')
             sys.stderr.write('''\n\tNo existe el archivo de configuración del log: %s\n''' % filename_log_ini)
             sys.exit(1)
 
-        logging.debug('This is a debug message (10)')
-        logging.info('This is an info message (20)')
-        logging.warning('This is a warning message (30)')
-        logging.error('This is an error message (40)')
-        logging.critical('This is a critical error message (50)')
+        self.log.debug('TEST: This is a debug message (10)')
+        self.log.info('TEST: This is an info message (20)')
+        self.log.warning('TEST: This is a warning message (30)')
+        self.log.error('TEST: This is an error message (40)')
+        self.log.critical('TEST: This is a critical error message (50)')
 
-        logging.debug('Class::__init__: Entrando a la clase ejemplo().')
+        self.log.debug('Class::__init__: Entrando a la clase ejemplo().')
 
     def __del__(self):
-        """Este metodo se invoca cuando se destruye la clase
+        """
+        Este método se invoca cuando se destruye la clase.
         """
 
-        print 'Saliendo de la clase ejemplo().'
+        # self.log.debug('Class::__del__: Entrando en la clase.')
+        pass
+        # self.log.debug('Class::__del__: Saliendo de la clase.')
+        pass
+
+    def usage(self):
+        """
+        Mostramos las opciones necesarias para que esta aplicación funcione.
+        """
+
+        # self.log.debug('Class::usage: Entrando en la clase.')
+        print "Usage: filter.py\n\t--bayes=bayes.pck\n\t--from=folder,folder,folder\n\t[--to=folder]\n\t[--detail]\n\t[--over=float|--under=float]"
+        print """\n\nExample:\n\tpython filter.py --from=/Personal/Hotmail,/Personal/ExJunk --over=.35 --detail --to=/SpamMaybe"""
+        # self.log.debug('Class::usage: Saliendo de la clase.')
+        return
 
     def metodo_de_ejemplo(self, nombre):
-        """Este metodo devuelve un saludo
         """
+        Este método devuelve un saludo, a modo de ejemplo básico.
+        """
+
+        self.log.debug('Class::metodo_de_ejemplo: Entrando en el metodo_de_ejemplo().')
 
         self.nombre = nombre
 
         print 'Mi nombre es %s' % nombre
+
+        self.log.debug('Class::metodo_de_ejemplo: Saliendo del metodo_de_ejemplo().')
 
 
 # Test de funcionalidad
@@ -140,3 +189,4 @@ if __name__ == '__main__':
     MI_NOMBRE = 'Luis'
     TEST = Ejemplo()
     TEST.metodo_de_ejemplo(MI_NOMBRE)
+    #TEST.log.info('Ejemplo::init un disoarate, zi zeñó')
